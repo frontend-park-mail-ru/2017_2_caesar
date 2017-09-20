cb471d3ce44403c1198c4a8d6bbc942d80cc19a8
podlovchenko <podlovchenko.vera@gmail.com>
2017-09-20 14:47:56 +0300

    base url

--- public/modules/http.js
+++ public/modules/http.js
@@ -19,7 +19,7 @@ class Http {
 
   static Post(address, body, callback) {
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', `https://caesar-server.herokuapp.com${address}`, true);
+    xhr.open('POST', `http://localhost:8081${address}`, true);
     xhr.withCredentials = true;
     xhr.setRequestHeader('Content-Type', 'application/json; charset=utf8');
 
