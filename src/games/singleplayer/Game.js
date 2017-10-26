9f639b9beaf4e0b757fe8b2e2bf67f4bf8010c66
podlovchenko <podlovchenko.vera@gmail.com>
2017-10-26 22:54:58 +0300

    update game

--- src/games/singleplayer/Game.js
+++ src/games/singleplayer/Game.js
@@ -8,6 +8,7 @@ import State from './State';
 
 const WORLD_WIDTH = window.innerWidth;
 const WORLD_HEIGHT = window.innerHeight;
+const WORLD_DEEP = 2000;
 
 const COINS = 40;
 const TEXT_COINS_X = WORLD_WIDTH - 120;
@@ -27,15 +28,19 @@ const GROUND_Y = 200;
 const GROUND_ITEM_WIDTH = 32;
 const GROUND_ITEM_HEIGHT = 32;
 
-const PLAYER_ITEM_WIDTH = 64;
-const PLAYER_ITEM_HEIGHT = 64;
+const PLAYER_ITEM_WIDTH = 32;
+const PLAYER_ITEM_HEIGHT = 32;
 const PLAYER_X = WORLD_WIDTH / 2;
 const PLAYER_Y = GROUND_Y - PLAYER_ITEM_HEIGHT;
 const PLAYER_VELOCITY_X = 150;
 const PLAYER_VELOCITY_Y = -250;
+const PLAYER_BOUNCE = 0;
+const PLAYER_GRAVITY = 300;
 
 const COINS_ICON_WIDTH = 32;
 const COINS_ICON_HEIGHT = 32;
+const COINS_ICON_X = 0;
+const COINS_ICON_Y = 0;
 
 const ICON_WIDTH = 64;
 const ICON_HEIGHT = 64;
@@ -67,15 +72,18 @@ class Game {
 
   preload() {
     this.game.load.image('sky', 'sprites/BG.png');
+    this.game.load.image('free', 'sprites/free.png');
     this.game.load.image('ground', 'sprites/ground.png');
-    this.game.load.image('dude', 'sprites/Skeleton.png');
+    this.game.load.image('ground-top', 'sprites/ground-top.png');
+    this.game.load.spritesheet('dude', 'sprites/dude.png', 32, 48);
+    // this.game.load.image('dude', 'sprites/dude.png');
     this.game.load.image('coin', 'sprites/coin.png');
     this.game.load.image('energy', 'sprites/energy.png');
     this.game.load.image('home', 'sprites/home.png');
   }
 
   create() {
-    const router = new Router();
+    this.game.world.setBounds(0, 0, this.game.world.width, WORLD_DEEP);
 
     this.state = new State();
 
@@ -85,10 +93,13 @@ class Game {
     this.bg.width = this.game.world.width;
     this.bg.height = this.game.world.height;
 
-    this.coins = this.game.add.group();
+    this.free = this.game.add.sprite(GROUND_X, GROUND_Y, 'free');
+    this.free.width = this.game.world.width - GROUND_X;
+    this.free.height = WORLD_DEEP - GROUND_Y;
 
+    this.coins = this.game.add.group();
     for (let i = 0; i < COINS; i++) {
-      this.coin = this.game.add.sprite(this.game.rnd.integerInRange(GROUND_X, WORLD_WIDTH), this.game.rnd.integerInRange(GROUND_Y, WORLD_HEIGHT), 'coin');
+      this.coin = this.game.add.sprite(this.game.rnd.integerInRange(GROUND_X, WORLD_WIDTH), this.game.rnd.integerInRange(GROUND_Y, WORLD_DEEP), 'coin');
       this.coin.width = COINS_ICON_WIDTH;
       this.coin.height = COINS_ICON_HEIGHT;
       this.game.physics.arcade.enable(this.coin);
@@ -97,26 +108,19 @@ class Game {
 
     this.platforms = this.game.add.group();
     this.platforms.enableBody = true;
-
-    this.home = this.game.add.sprite(0, 0, 'home');
-    this.home.width = ICON_WIDTH;
-    this.home.height = ICON_HEIGHT;
-    this.home.inputEnabled = true;
-    this.home.events.onInputDown.add(() => {
-      router.go('/');
-    }, this);
-
     for (let i = GROUND_X; i < this.game.world.width; i += GROUND_ITEM_WIDTH) {
       for (let j = GROUND_Y; j < this.game.world.height; j += GROUND_ITEM_HEIGHT) {
-        this.ground = this.platforms.create(i, j, 'ground');
+        j === GROUND_Y ? this.ground = this.platforms.create(i, j, 'ground-top') : this.ground = this.platforms.create(i, j, 'ground');
         this.ground.width = GROUND_ITEM_WIDTH;
         this.ground.height = GROUND_ITEM_HEIGHT;
         this.ground.body.immovable = true;
         this.ground.inputEnabled = true;
         this.ground.input.useHandCursor = true;
         this.ground.events.onInputDown.add((sprite) => {
-          sprite.destroy();
-          this.state.decreaseEnergy();
+          if (sprite.centerX - this.player.centerX < 2 * PLAYER_ITEM_WIDTH && sprite.centerY - this.player.centerY < 2 * PLAYER_ITEM_HEIGHT) {
+            sprite.destroy();
+            this.state.decreaseEnergy();
+          }
         }, this);
       }
     }
@@ -124,11 +128,15 @@ class Game {
     this.player = this.game.add.sprite(PLAYER_X, PLAYER_Y, 'dude');
     this.player.width = PLAYER_ITEM_WIDTH;
     this.player.height = PLAYER_ITEM_HEIGHT;
+    this.game.camera.follow(this.player);
     this.game.physics.arcade.enable(this.player);
 
-    this.player.body.bounce.y = 0.2;
-    this.player.body.gravity.y = 300;
+    this.player.body.bounce.y = PLAYER_BOUNCE;
+    this.player.body.gravity.y = PLAYER_GRAVITY;
     this.player.body.collideWorldBounds = true;
+
+    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
@@ -136,44 +144,75 @@ class Game {
 
     this.textCoin = this.game.add.text(TEXT_COINS_X, TEXT_COINS_Y,
       this.state.coins, this.styleText);
+    this.textCoin.fixedToCamera = true;
 
     this.iconCoin = this.game.add.sprite(TEXT_COINS_ICON_X, TEXT_COINS_Y, 'coin');
     this.iconCoin.width = TEXT_COINS_ICON_WIDTH;
     this.iconCoin.height = TEXT_COINS_ICON_HEIGHT;
+    this.iconCoin.fixedToCamera = true;
 
     this.textEnergy = this.game.add.text(TEXT_ENERGY_X, TEXT_ENERGY_Y,
       this.state.energy, this.styleText);
+    this.textEnergy.fixedToCamera = true;
 
     this.iconEnergy = this.game.add.sprite(TEXT_ENERGY_ICON_X, TEXT_ENERGY_Y, 'energy');
     this.iconEnergy.width = TEXT_ENERGY_ICON_WIDTH;
     this.iconEnergy.height = TEXT_ENERGY_ICON_HEIGHT;
+    this.iconEnergy.fixedToCamera = true;
+
+    this.home = this.game.add.sprite(COINS_ICON_X, COINS_ICON_Y, 'home');
+    this.home.width = ICON_WIDTH;
+    this.home.height = ICON_HEIGHT;
+    this.home.inputEnabled = true;
+    this.home.fixedToCamera = true;
+    this.home.events.onInputDown.add(() => {
+      const router = new Router();
+      router.go('/');
+    }, this);
+
+    this.showCoins = () => {
+      this.game.world.bringToTop(this.platforms);
+    };
+
+    this.search = () => {
+      this.game.world.bringToTop(this.coins);
+      this.game.time.events.add(Phaser.Timer.SECOND, this.showCoins, this);
+    };
+
+    this.keySearch = this.game.input.keyboard.addKey(Phaser.Keyboard.E);
+    this.keySearch.onDown.add(this.search, this);
+
+    console.log(this.textCoin);
   }
 
   update() {
-    this.textEnergy.destroy();
-    this.textEnergy = this.game.add.text(TEXT_ENERGY_X, TEXT_ENERGY_Y,
-      this.state.energy, this.styleText);
+    this.textEnergy.setText(this.state.energy);
+    this.textCoin.setText(this.state.coins);
 
     this.player.body.velocity.x = 0;
 
-    this.hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
+    if (Game.isPlayed) {
+      this.hitPlatform = this.game.physics.arcade.collide(this.player, this.platforms);
 
-    this.game.physics.arcade.overlap(this.player, this.coins, (player, coin) => {
-      coin.kill();
-      this.state.increaseCoins();
-      this.textCoin.destroy();
-      this.textCoin = this.game.add.text(TEXT_COINS_X, TEXT_COINS_Y,
-        this.state.coins, this.styleText);
-    });
+      this.game.physics.arcade.overlap(this.player, this.coins, (player, coin) => {
+        coin.kill();
+        this.state.increaseCoins();
+      });
 
-    if (this.cursors.left.isDown) {
-      this.player.body.velocity.x = -PLAYER_VELOCITY_X;
-    } else if (this.cursors.right.isDown) {
-      this.player.body.velocity.x = PLAYER_VELOCITY_X;
-    }
+      if (this.cursors.left.isDown) {
+        this.player.body.velocity.x = -PLAYER_VELOCITY_X;
+        this.player.animations.play('left');
+      } else if (this.cursors.right.isDown) {
+        this.player.body.velocity.x = PLAYER_VELOCITY_X;
+        this.player.animations.play('right');
+      } else {
+        this.player.animations.stop();
+        this.player.frame = 4;
+      }
 
-    if (this.cursors.up.isDown && this.player.body.touching.down && this.hitPlatform) {
-      this.player.body.velocity.y = PLAYER_VELOCITY_Y;
+      if (this.cursors.up.isDown && this.player.body.touching.down && this.hitPlatform) {
+        this.player.body.velocity.y = PLAYER_VELOCITY_Y;
+      }
     }
   }
 
